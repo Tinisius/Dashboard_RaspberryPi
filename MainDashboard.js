3c05c9f97e1a7d9c064c44065c27d4f2a71afe39
pibit pibit <tinisalemmealonso@gmail.com>
2026-05-13 16:42:13 -0300

    reconecto cada 5 min

--- MainDashboard.js
+++ MainDashboard.js
@@ -1,16 +1,28 @@
 import { io } from "socket.io-client";
 import { getStats } from "./system.js";
 
+const sleep = (sec) =>
+  new Promise((resolve) => setTimeout(resolve, sec * 1000));
+
 const socket = io("https://tinisius.site");
 
 socket.on("connect", () => {
   console.log("Conectado al servidor");
 });
 
-console.log("emito conexion rasp:");
-socket.emit("raspi_conn", (conn) => {
-  console.log(conn);
-});
+async function KeepConnection() {
+  while (true) {
+    console.log("reconectando...");
+    socket.emit("raspi_conn", (conn) => {
+      console.log(conn);
+    });
+    //espera 5 min
+    await sleep(300);
+  }
+}
+
+//se revive el socket cada 5min
+KeepConnection();
 
 socket.on("fetchResources", async (callback) => {
   console.log("me hicieron una request");
