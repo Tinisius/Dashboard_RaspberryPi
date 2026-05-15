e0d9ca84eb3796d04e30a68d714f1fa0d15d6fa1
pibit pibit <tinisalemmealonso@gmail.com>
2026-05-15 01:21:19 -0300

    agrego toda la logica de prender/apagar servidor + ver estado actual

--- MainDashboard.js
+++ MainDashboard.js
@@ -1,14 +1,85 @@
 import { io } from "socket.io-client";
 import { getStats } from "./system.js";
+import { Socket } from "socket.io";
+import { spawn } from "child_process";
+
+let serverState = "off";
+let serverProcess = null;
 
 const sleep = (sec) =>
   new Promise((resolve) => setTimeout(resolve, sec * 1000));
 
-const socket = io("https://tinisius.site");
+//---------------------------------------------------------------------------------------
+/*
+function testStart() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 5000);
+  });
+}
+function testStop() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 5000);
+  });
+}
+*/
 
-socket.on("connect", () => {
-  console.log("Conectado al servidor");
-});
+//devuelve una promesa que se resuelve al iniciar el server
+function startServer() {
+  return new Promise((resolve, reject) => {
+    //levantamos una terminal empezamos a iniciar el server
+    serverProcess = spawn("bash", ["-c", "~/Desktop/NeoForge-21.1/run.sh"], {
+      shell: true,
+    });
+    //generamos un listener por cada vez que se escribe algo en la terminal (STanDard OUTput)
+    serverProcess.stdout.on("data", (data) => {
+      //convertimos el Buffer Object a string
+      const text = data.toString();
+
+      console.log(text);
+
+      // Detectar cuando termina y resuelve la promesa
+      if (text.includes('For help, type "help"')) {
+        resolve();
+      }
+    });
+
+    //al encontrar errores, muestra por pantalla (puede que lo borre)
+    serverProcess.stderr.on("data", (data) => {
+      console.error(data.toString());
+    });
+    //al encontrar un error rechaza
+    serverProcess.on("error", reject);
+  });
+}
+
+//devuelve una promesa que se resuelve al apagar el server
+function stopServer() {
+  return new Promise((resolve) => {
+    //evitamos apagarlo si ya esta apagado
+    if (!serverProcess) {
+      resolve();
+      return;
+    }
+
+    // Esperar cierre completo
+    serverProcess.on("close", () => {
+      console.log("🔴 Server apagado");
+
+      // IMPORTANTE
+      serverProcess = null;
+
+      resolve();
+    });
+
+    //cerramos el server
+    serverProcess.stdin.write("stop\n");
+  });
+}
+//---------------------------------------------------------------------------------------
 
 async function KeepConnection() {
   while (true) {
@@ -21,11 +92,54 @@ async function KeepConnection() {
   }
 }
 
+//const socket = io("https://tinisius.site");
+const socket = io("http://localhost:8000");
+
+socket.on("connect", () => {
+  console.log("Conectado al servidor");
+});
 //se revive el socket cada 5min
 KeepConnection();
 
+//piden recursos, devuelvo
 socket.on("fetchResources", async (callback) => {
-  console.log("me hicieron una request");
+  console.log("me hicieron una request resources");
 
   callback(await getStats());
+});
+
+//piden estado del sv, devuelvo
+socket.on("fetchState", async (callback) => {
+  console.log("me hicieron una request state");
+  callback(serverState); //devuelve
+});
+
+socket.on("changeState", async (newState) => {
+  //valida no molestar los procesos pendientes
+  if (newState === "starting" || newState === "stoping") return;
+
+  //valida no prender lo prendido o apagar lo apagado
+  if (serverState !== newState) {
+    try {
+      //iniciar el server
+      if (newState === "started") {
+        socket.emit("update_sv", "starting");
+        serverState = "starting";
+        await startServer();
+        socket.emit("update_sv", "started");
+        serverState = "started";
+      }
+      //apagar el server
+      else if (newState === "off") {
+        socket.emit("update_sv", "closing");
+        serverState = "closing";
+        await stopServer();
+        socket.emit("update_sv", "off");
+        serverState = "off";
+      }
+    } catch (error) {
+      console.error("Error prendiendo/apagando", error);
+      socket.emit("update_sv", "error");
+    }
+  }
 });
