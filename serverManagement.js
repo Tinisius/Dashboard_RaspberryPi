5688a0e1453011c9d00815ff7ecd75532b0b9088
Tinisius <tinisalemmealonso@gmail.com>
2026-07-10 02:10:27 -0300

    modifico logs

--- serverManagement.js
+++ serverManagement.js
@@ -11,7 +11,7 @@ export let sv_data = {
   players: [],
   startedAt: null,
   timeOut: 0,
-  logs: ["inicial"],
+  logs: [""],
 };
 
 async function startIdleTimeout(time = 300) {
@@ -122,7 +122,7 @@ export function startServer() {
     //guardo el path de la carpeta del server NeoForge
     const serverPath = `${os.homedir()}/Desktop/Forge-1.20.1`;
 
-    sv_data.logs = ["inicial"]; //borramos los logs viejos
+    sv_data.logs = [""]; //borramos los logs viejos
 
     //levantamos una terminal y empezamos a iniciar el server
     serverProcess = spawn("./run.sh", {
@@ -133,10 +133,9 @@ export function startServer() {
       //convertimos el Buffer Object a string
       const text = data.toString();
 
-      manageLog(text);
-
       // Detectar cuando termina y resuelve la promesa
       if (text.includes('For help, type "help"')) {
+        manageLog(text);
         serverListener(); //queda en background escuchando todos los eventos del server
         resolve();
       }
